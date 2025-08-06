2309470525f70b57448d2cb98fa9aeffd4ece2b5
Priyanshu Goel <priyanshu.11goel@gmail.com>
2025-08-06 14:47:15 +0530

    Update PlayerParticipators.java

--- PlayerParticipators.java
+++ PlayerParticipators.java
@@ -1,10 +1,27 @@
 class PlayerParticipators{
-    int PlayerNum;    //Instance variable
+    String name;
+    int score = 0;
+    private Scanner sc;
 
-    public int PlayerNumber(int PlayerId){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Player " + PlayerId + ", enter your guess: ");
-        PlayerNum = sc.nextInt();
-        return PlayerNum;
+    PlayerParticipators(String name, Scanner sc) {
+        this.name = name;
+        this.sc = sc;
+    }
+    public int PlayerNumber(){
+         int guess;
+        do {
+            System.out.print(name + ", Enter your guess (0-100): ");
+            while (!sc.hasNextInt()) {
+                System.out.print("Invalid input! Enter a valid number: ");
+                sc.next(); // clear invalid input
+            }
+            guess = sc.nextInt();
+        } while (guess < 0 || guess > 100);
+
+        return guess;
+    }
+
+    public void increaseScore() {
+        score++;
     }
 } 
